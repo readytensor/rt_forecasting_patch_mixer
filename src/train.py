2c1622abcc00797d6537452b58a1697ba25423e0
Mo <mohamedabdelhamid3030@gmail.com>
2024-12-26 18:23:48 +0200

    Update hyperparameters to match package defaults

--- src/train.py
+++ src/train.py
@@ -42,7 +42,7 @@ def run_training(
     """
 
     try:
-        with ResourceTracker(logger=logger, monitoring_interval=0.1):
+        with ResourceTracker(logger=logger, monitoring_interval=0.1, is_train=True):
             logger.info("Starting training...")
             # load and save schema
             logger.info("Loading and saving schema...")
