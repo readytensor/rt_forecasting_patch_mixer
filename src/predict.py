08d8a975cd1cec34d629e74ce40d103761f5f123
Mo <mohamedabdelhamid3030@gmail.com>
2024-12-26 18:30:45 +0200

    Update hyperparameters to match package defaults

--- src/predict.py
+++ src/predict.py
@@ -91,7 +91,7 @@ def run_batch_predictions(
     """
 
     try:
-        with ResourceTracker(logger=logger, monitoring_interval=0.1):
+        with ResourceTracker(logger=logger, monitoring_interval=0.1, is_train=False):
             logger.info("Making batch predictions...")
 
             logger.info("Loading schema...")
